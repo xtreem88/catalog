36349dc5098157844456638bff659ccee0d00fe8
Segun <xtreem88@gmail.com>
2015-08-24 23:35:28 +0100

    2nd iteration... form creation

--- finalProject.py
+++ finalProject.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -10,49 +10,78 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+# Fake Restaurants
+restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
+
+restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {
+    'name': 'Blue Burgers', 'id': '2'}, {'name': 'Taco Hut', 'id': '3'}]
+
+
+# Fake Menu Items
+items = [{'name': 'Cheese Pizza', 'description': 'made with fresh cheese',
+          'price': '$5.99', 'course': 'Entree', 'id': '1'},
+         {'name': 'Chocolate Cake', 'description': 'made with Dutch Chocolate',
+          'price': '$3.99', 'course': 'Dessert', 'id': '2'},
+         {'name': 'Caesar Salad', 'description':
+          'with fresh organic vegetables', 'price': '$5.99', 'course': 'Entree',
+          'id': '3'},
+         {'name': 'Iced Tea', 'description': 'with lemon',
+          'price': '$.99', 'course': 'Beverage', 'id': '4'},
+         {'name': 'Spinach Dip', 'description': 'creamy dip with fresh spinach',
+          'price': '$1.99', 'course': 'Appetizer', 'id': '5'}]
+item = {'name': 'Cheese Pizza', 'description': 'made with fresh cheese',
+        'price': '$5.99', 'course': 'Entree', 'id': '1'}
 
 
 @app.route('/')
 @app.route('/restaurant/')
 def showRestaurants():
-
-    return "This page would show all my restaurants"
+    # return "This page would show all my restaurants"
+    return render_template('restaurant.html', restaurants=restaurants)
 
 
 @app.route('/restaurant/new')
 def newRestaurant():
-    return "This page will be for making a new restaurant"
+    # return "This page will be for making a new restaurant"
+    return render_template('newrestaurant.html')
 
 
 @app.route('/restaurant/<int:restaurant_id>/edit')
 def editRestaurant(restaurant_id):
-    return "This page will be for editing restaurant %s" % restaurant_id
+    # return "This page will be for editing restaurant %s" % restaurant_id
+    return render_template('editrestaurant.html', restaurant=restaurant, restaurant_id=restaurant_id)
 
 
 @app.route('/restaurant/<int:restaurant_id>/delete')
 def deleteRestaurant(restaurant_id):
-    return "This page will be for deleting restaurant %s" % restaurant_id
+    # return "This page will be for deleting restaurant %s" % restaurant_id
+    return render_template('deleterestaurant.html', restaurant=restaurant, restaurant_id=restaurant_id)
 
 
 @app.route('/restaurant/<int:restaurant_id>')
 @app.route('/restaurant/<int:restaurant_id>/menu')
 def showMenu(restaurant_id):
-    return "This page is the menu for restaurant %s" % restaurant_id
+    # return "This page is the menu for restaurant %s" % restaurant_id
+    return render_template('menu.html', restaurant=restaurant, items=items)
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/new')
 def newMenuItem(restaurant_id):
-    return "This page is for making a new menu item for restaurant %s" % restaurant_id
+    # return "This page is for making a new menu item for restaurant %s" %
+    # restaurant_id
+    return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit')
 def editMenuItem(restaurant_id, menu_id):
-    return "This page is for editing menu item %s" % menu_id
+    # return "This page is for editing menu item %s" % menu_id
+    return render_template('editmenuitem.html', restaurant_id=restaurant_id, item=item)
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete')
-def deleteMenuItem(restaurant_id,menu_id):
-    return "This page is for deleting menu item %s" % menu_id
+def deleteMenuItem(restaurant_id, menu_id):
+    # return "This page is for deleting menu item %s" % menu_id
+    return render_template('deletemenuitem.html', restaurant_id=restaurant_id, item=item)
 
 
 if __name__ == '__main__':
